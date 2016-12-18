7cc5905af9f16e086e356414940b98795d796e94
xinzhang <xinzhang@thoughtworks.com>
2016-12-18 20:02:00 +0800

    init restful api

---
+++ src/main/java/com/demo/LoginApp.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class LoginApp {
+    public static void main(String[] args) {
+        SpringApplication.run(LoginApp.class, args);
+    }
+}
---
+++ src/main/java/com/demo/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class LoginController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+}
